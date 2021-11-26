5a9115c759544717baf402f75692df020a20cde1
vhdttdhv <72727639+vhdttdhv@users.noreply.github.com>
2021-11-25 18:00:54 -0800

    Change default theme to dark mode

--- src/redux/themeSlice.js
+++ src/redux/themeSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const themeSlice = createSlice({
   name: "theme",
   initialState: {
-    value: "light",
+    value: "dark",
   },
   reducers: {
     changeTheme: (state) => {
